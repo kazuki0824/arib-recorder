bf63c65b8e43bc28f4893ba7f849a2dc048f8934
kazuki0824 <kazukuro0824@gmail.com>
2022-11-20 03:26:36 +0900

    [BUG] state transition

--- recorder/src/recording_pool/recording_task.rs
+++ recorder/src/recording_pool/recording_task.rs
@@ -18,24 +18,31 @@ mod eit_parser;
 mod io_object;
 
 machine!(
-    #[derive(Clone, Copy, PartialEq)]
+    #[derive(Clone, Copy, PartialEq, Debug)]
     pub(crate) enum RecordingState {
-        A { since: DateTime<Local> },
-        B1 { since: DateTime<Local> },
-        B2 { since: DateTime<Local> },
-        Rec { since: DateTime<Local>, end: DateTime<Local> },
+        A {
+            since: DateTime<Local>,
+        },
+        B1 {
+            since: DateTime<Local>,
+        },
+        B2 {
+            since: DateTime<Local>,
+        },
+        Rec {
+            since: DateTime<Local>,
+            end: DateTime<Local>,
+        },
         Timeout {},
         Success {},
     }
 );
 impl IntoB2 for A {}
 impl IntoB2 for B1 {}
-impl IntoB2 for B2 {}
 
 impl IntoRec for A {}
 impl IntoRec for B1 {}
 impl IntoRec for B2 {}
-impl IntoRec for Rec {}
 
 trait IntoB2 {
     fn on_found_in_following(self, _: FoundInFollowing) -> B2
@@ -49,13 +56,13 @@ trait IntoB2 {
 }
 
 trait IntoRec {
-    fn on_found_in_present(self, _: FoundInPresent) -> Rec
+    fn on_found_in_present(self, present: FoundInPresent) -> Rec
     where
         Self: Sized,
     {
         Rec {
             since: Local::now(),
-            end: Local::now(),
+            end: present.duration.map_or(Local::now() + Duration::minutes(5), |d| present.start_at + d),
         }
     }
 }
@@ -82,7 +89,15 @@ impl B1 {
         }
     }
 }
+impl B2 {
+    fn on_found_in_following(self, _: FoundInFollowing) -> B2 {
+        self
+    }
+}
 impl Rec {
+    fn on_found_in_present(self, _: FoundInPresent) -> Rec {
+        self
+    }
     fn on_wait_for_premiere(self, WaitForPremiere { start_at }: WaitForPremiere) -> Success {
         Success {}
     }
@@ -143,13 +158,13 @@ impl RecordingTask {
         // Specify file name here
         file_location.push(format!(
             "{}_{}.m2ts-tmp",
-            info.program.id,
+            info.program.event_id,
             info.program
                 .name
                 .as_ref()
                 .unwrap_or(&"untitled".to_string())
         ));
-        let target = Some(IoObject::new(Some(file_location.as_path())).await?);
+        let target = Some(IoObject::new(None).await?);
         Ok(Self {
             target,
             eit: EitParser::new(),
@@ -200,27 +215,44 @@ impl AsyncWrite for RecordingTask {
             *me.next_state = after;
 
             if me.state != me.next_state {
+                info!(
+                    "[id={}] Transition from {:?} to {:?}",
+                    item.program.id, me.state, me.next_state
+                );
                 // Determine file name
                 let new_path = match me.next_state {
-                    RecordingState::Rec(_) if me.file_location.set_extension("m2ts") => Some(me.file_location.as_path()),
-                    RecordingState::B1(_) | RecordingState::B2(_) if me.file_location.set_extension("m2ts-tmp") => Some(me.file_location.as_path()),
+                    RecordingState::Rec(_) if me.file_location.set_extension("m2ts") => {
+                        Some(me.file_location.as_path())
+                    }
+                    RecordingState::B1(_) | RecordingState::B2(_)
+                        if me.file_location.set_extension("m2ts-tmp") =>
+                    {
+                        Some(me.file_location.as_path())
+                    }
                     _ => None,
                 };
+                *me.state = *me.next_state;
 
                 // Kill the current IoObject and create a new one
                 std::thread::scope(|s| {
                     let w = cx.waker().clone();
 
-                    s.spawn(|| async {
-                        let new_writer = IoObject::new(new_path).await.unwrap();
-                        if let Some(mut old_writer) = me.target.replace(new_writer) {
-                            old_writer.shutdown().await.unwrap()
-                        }
-                        info!(
-                            "[id={}] Save location has been changed to {:?}.",
-                            item.program.id, new_path
-                        );
-                        w.wake()
+                    s.spawn(|| {
+                        // Create the runtime
+                        let rt = tokio::runtime::Runtime::new().unwrap();
+                        // Execute the future, blocking the current thread until completion
+                        let future = async {
+                            let new_writer = IoObject::new(new_path).await.unwrap();
+                            if let Some(mut old_writer) = me.target.replace(new_writer) {
+                                old_writer.shutdown().await.unwrap()
+                            }
+                            info!(
+                                "[id={}] Save location has been changed to {:?}.",
+                                item.program.id, new_path
+                            );
+                            w.wake()
+                        };
+                        rt.block_on(future)
                     });
                 });
 
