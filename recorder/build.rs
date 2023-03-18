741386736a1f2de7a81652a3cdb0910b66f2eb31
maleicacid <kazukuro0824+dev@gmail.com>
2023-03-19 03:13:57 +0900

    Fix protoc-compiler options

--- recorder/build.rs
+++ recorder/build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("./proto/page.proto")?;
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .build_transport(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["proto/page.proto"], &["proto"])?;
+
     Ok(())
 }
